7b9da2e9cdf10a2b915b080fa5a3fff590b14aaf
Victor <victor.liger0800@gmail.com>
2024-07-18 22:10:56 -0300

    feat: Implementando categorias do card

---
+++ src/app/Components/notice-card/notice-card.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-notice-card',
+  standalone: true,
+  imports: [
+    CommonModule
+  ],
+  templateUrl: './notice-card.component.html',
+  styleUrl: './notice-card.component.css'
+})
+export class NoticeCardComponent implements OnInit{
+
+
+  imageURL:string = '';
+
+  description : string[] = [
+    'Design',
+    'Product',
+    'Reseach',
+    'Interviews',
+    'Frameworks'
+  ]
+
+  cardDescription : string[] = []
+
+
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+      this.selectRandomDescriptions()
+    
+  }
+
+  selectRandomDescriptions(): void {
+
+
+      let numbersDescription = Math.random()*this.description.length
+
+      for(let iterator = 0; iterator < numbersDescription; iterator++){
+
+        const randomIndex = Math.floor(Math.random() * this.description.length);
+
+        const descriptionIndex = this.cardDescription.indexOf(this.description[iterator])
+
+        this.cardDescription.push(this.description[iterator])
+
+        
+      }
+    }
+
+}
