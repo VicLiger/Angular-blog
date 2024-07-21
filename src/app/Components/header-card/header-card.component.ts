75671773cb9c95d6d61cb7dfc16bd4b9137addff
Victor <victor.liger0800@gmail.com>
2024-07-21 00:17:53 -0300

    feat: terminando barra de opções

--- src/app/Components/header-card/header-card.component.ts
+++ src/app/Components/header-card/header-card.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header-card',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header-card.component.html',
   styleUrl: './header-card.component.css'
 })
-export class HeaderCardComponent {
+export class HeaderCardComponent implements OnInit{
+
+
+  headerOptions:string[] = [
+    "Home",
+    "Products",
+    "Solutions",
+    "Pricing",
+    "Resources",
+    "Company",
+    "Careers"
+  ]
+  constructor() {
+    
+  }
+  ngOnInit(): void {
+    
+  }
+
 
 }
