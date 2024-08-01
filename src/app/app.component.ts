94d5f7c5d8648b29a112dc7fc959a37944a31842
Victor <victor.liger0800@gmail.com>
2024-07-31 22:33:23 -0300

    🚧 feat: Construindo header principal com conteúdo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NoticeCardComponent } from "./Components/notice-card/notice-card.component";
 import { HeaderCardComponent } from "./Components/header-card/header-card.component";
+import { HeaderTextComponent } from "./Components/header-text/header-text.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NoticeCardComponent, HeaderCardComponent],
+  imports: [RouterOutlet, NoticeCardComponent, HeaderCardComponent, HeaderTextComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
